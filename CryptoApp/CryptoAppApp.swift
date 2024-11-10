5d118e65d0faef67bb2619bd37d68a49568cf1b6
SergeyZakurakin <pricezak@gmail.com>
2024-11-10 13:17:00 -0500

    setup project

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Sergey Zakurakin on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
