2eec24e690098f305d0ec844c7b0999ea009d876
SergeyZakurakin <pricezak@gmail.com>
2024-11-16 21:32:59 -0500

    create fileManager

--- CryptoApp/Services/CoinImageService.swift
+++ CryptoApp/Services/CoinImageService.swift
@@ -14,13 +14,28 @@ final class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.shared
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrieved Image from file manager!")
+        } else {
+            downloadCoinImage()
+            print("downloading image now")
+        }
+    }
+    
+    private func downloadCoinImage() {
+        print("downloading image now")
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkingManager.download(url: url)
@@ -31,10 +46,11 @@ final class CoinImageService {
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self]
                 (returnedImage) in
                 
-                guard let self else { return }
-                image = returnedImage
+                guard let self, let downloadedImage = returnedImage else { return }
+                image = downloadedImage
                 // cancel get request after once
                 imageSubscription?.cancel()
+                fileManager.saveImage(image: downloadedImage, imageName: imageName, folderName: folderName)
             })
     }
 }
